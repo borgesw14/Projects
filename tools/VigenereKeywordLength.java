8eb574d809df574b3ba1a0f4881c7aa4ec9f3114
borgesw14 <borgesw14@gmail.com>
2021-09-15 08:23:12 -0400

    egetSequence now find all seq up to a value

--- tools/VigenereKeywordLength.java
+++ tools/VigenereKeywordLength.java
@@ -5,28 +5,32 @@ import java.util.ArrayList;
 public class VigenereKeywordLength {
     public static int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 
+
+
     /* 
     getSequences takes a cyphertext string and breaks it up into a certeain number of sequences based on probable keylengths.
     */
     
     public ArrayList<ArrayList<String>> getSequences(String cypherText) {
         ArrayList<ArrayList<String>> sequences = new ArrayList<ArrayList<String>>();
+        int numOfSequences = 15;
 
-        for (int i = 0; i < primes[i]; i++) {
 
-            if (cypherText.length()/primes[i] < primes[i]) {
+        for (int i = 2; i < 15; i++) {
+
+            /* if (cypherText.length()/2 < primes[i]) {
                 break;
-            }
+            } */
 
             //initialize arraylist with strings
             ArrayList<String> sequence = new ArrayList<String>();
-            for (int j = 0; j < primes[i]; j++) {
+            for (int j = 0; j < i; j++) {
                 sequence.add("");
             }
 
             for (int j = 0; j < cypherText.length(); j++) {
                 //first index
-                int sequenceIndex = (j+1) % primes[i];
+                int sequenceIndex = (j+1) % i;
 
                 String currentSequence = sequence.remove(sequenceIndex);
                 currentSequence = currentSequence + cypherText.charAt(j);
@@ -54,7 +58,7 @@ public class VigenereKeywordLength {
 
     public static void main(String[] args) {
         
-        String cypherText = "WMXSCHDSLTGKTGRBNKNEkOSEGIEGXHHXORIERCKRRIGRNKXOHQGAAAGGFLMFCSWUSZOIWONNYKEHHRCNBSORNAYEFFETSOGCINIFXIIXETKOKIRRXLRRIOEZIAPDTNCCSUTXXSYTYNEERMMMDHUMRSAMUCHNEFYSDANHTEROCKXSLUNMWAIGSLYNESRRUEEKDIEILTHT";
+        String cypherText = "vptzmdrttzysubxaykkwcjmgjmgpwreqeoiivppalrujtlrzpchljftupucywvsyiuuwufirtaxagfpaxzxjqnhbfjvqibxzpotciiaxahmevmmagyczpjxvtndyeuknulvvpbrptygzilbkeppyetvmgpxuknulvjhzdtgrgapygzrptymevppaxygkxwlvtiawlrdmipweqbhpqgngioirnxwhfvvawpjkglxamjewbwpvvmafnlojalh";
 
         cypherText = cypherText.toLowerCase();
         cypherText = cypherText.replaceAll("//s", "");
@@ -67,9 +71,9 @@ public class VigenereKeywordLength {
 
             System.out.println("if key length were: " + sequences.get(i).size());
 
-            for (int j = 0; j < sequences.get(i).size(); j++) {
+            /* for (int j = 0; j < sequences.get(i).size(); j++) {
                 System.out.println(sequences.get(i).get(j));
-            }
+            } */
 
             System.out.println(vkl.getAvgIC(sequences.get(i)));
 
