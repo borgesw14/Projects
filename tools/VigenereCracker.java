4f5b9ab82bb743d6664fa4b72f41165808d933e6
borgesw14 <borgesw14@gmail.com>
2021-09-15 10:17:57 -0400

    got it to generate all possible ceaser ciphers

--- tools/VigenereCracker.java
+++ tools/VigenereCracker.java
@@ -2,7 +2,39 @@ package tools;
 
 public class VigenereCracker {
     
+    public static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+    /* 
+    character frequencies for the english language extracted from:
+    http://cs.wellesley.edu/~fturbak/codman/letterfreq.html
+    */
+    public static double[] charFreqEng = {0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150, 0.01974, 0.00074};
+
+    public static String[] generateCeaserCiphers(String sequence) {
+        String[] ceasersCiphers = new String[26];
+
+        for (int i = 0; i < 26; i++) {
+            StringBuilder result = new StringBuilder();
+            for (char character : sequence.toCharArray()) {
+            
+                int originalAlphabetPosition = character - 'a';
+                int newAlphabetPosition = (originalAlphabetPosition + i) % 26;
+                char newCharacter = (char) ('a' + newAlphabetPosition);
+                result.append(newCharacter);
+            
+            }
+
+            ceasersCiphers[i] = result.toString();
+        }
+        return ceasersCiphers;
+    }
+
     public static void main(String[] args) {
-        
+        String str = "zkrolyhvlqdslqdsoh";
+        String[] ciphers = generateCeaserCiphers(str);
+
+        for (String string : ciphers) {
+            System.out.println(string);
+        }
     }
 }
--- tools/VigenereKeywordLength.java
+++ tools/VigenereKeywordLength.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class VigenereKeywordLength {
     public static int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-
     public static int numOfSequences = 15;
 
     /* 
@@ -113,6 +112,6 @@ public class VigenereKeywordLength {
             System.out.print(integer+ " ");
         }
 
-        
+
     }
 }
