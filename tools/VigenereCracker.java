15b92adeccc49b1db64179010bf69a6566690cde
borgesw14 <borgesw14@gmail.com>
2021-09-15 10:27:59 -0400

    format output and set up chi-squared statistic

--- tools/VigenereCracker.java
+++ tools/VigenereCracker.java
@@ -10,6 +10,10 @@ public class VigenereCracker {
     */
     public static double[] charFreqEng = {0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150, 0.01974, 0.00074};
 
+
+    /* 
+        generates all 26 possible ceaser ciphers from the sequence
+    */
     public static String[] generateCeaserCiphers(String sequence) {
         String[] ceasersCiphers = new String[26];
 
@@ -29,12 +33,23 @@ public class VigenereCracker {
         return ceasersCiphers;
     }
 
+    /* 
+        A program to determine the chi-squared statistic of a string of text in relation to english.
+        input: a string of text
+        output: chi-squared statistic
+    */
+    public static double getChiSqrd(String currentCipher) {
+        double chisqrd = 0;
+
+        return chisqrd;
+    }
+
     public static void main(String[] args) {
         String str = "zkrolyhvlqdslqdsoh";
         String[] ciphers = generateCeaserCiphers(str);
 
-        for (String string : ciphers) {
-            System.out.println(string);
+        for (int i = 0; i < ciphers.length; i++) {
+            System.out.println(i + ": \t" + ciphers[i]);
         }
     }
 }
