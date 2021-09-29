0839cae44bda40830fc6da23b6922ab84e8b5900
William Borges <borgesw14@gmail.com>
2021-09-28 22:11:34 -0400

    i turned broke code to fixed code

--- data/scripts/convertGermanEngrams.py
+++ data/scripts/convertGermanEngrams.py
@@ -3,17 +3,23 @@ import os, re, sys, getopt
 def main(argv):
     inputfile = ''
     outputfile = ''
+    
     try:
         opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+
     except getopt.GetoptError:
         print('test.py -i <inputfile> -o <outputfile>')
         sys.exit(2)
+
     for opt, arg in opts:
+
         if opt == '-h':
             print ('test.py -i <inputfile> -o <outputfile>')
             sys.exit()
+
         elif opt in ("-i", "--ifile"):
             inputfile = arg
+
         elif opt in ("-o", "--ofile"):
             outputfile = arg
    
@@ -29,34 +35,25 @@ def main(argv):
 
         gerChar = ["Ä", "Ü", "Ö"]
         engChar = ["A", "U", "O"]
-
-        for line in data:
-            x = re.split("\s", line)
-
-            keys.append(x[0])
-            values.append(x[1])
-        
-        
+       
 
         for j,c in enumerate(gerChar):
-            for i, key in enumerate(keys):
-                keys[i] = key.replace(key, re.sub(c, engChar[j], key))
+            for i, key in enumerate(data):
+                data[i] = key.replace(key, re.sub(c, engChar[j], key))
 
-
-        for i in range(len(keys)):
+        for i, line in enumerate(data):
+            pair = re.split("\s", line)
+            keys.append(pair[0])
+            values.append(int(pair[1]))
             
-            for j in range(i+1, len(keys)):
-                
-                if(keys[i] == keys[j]):
-                    sum = int(values[i]) + int(values[j])
-
-                    values[i] = sum
-                    values[j] = sum
-
-        [newKeys.append(x) for x in keys if x not in newKeys]
-        [newValues.append(int(x)) for x in values if x not in newValues]
-
-        res = {newKeys[i]: newValues[i] for i in range(len(newKeys))}
+        for i, key in enumerate(keys):
+            if(key not in newKeys):
+                newKeys.append(key)
+                newValues.append(values[i])
+            else:
+                newValues[newKeys.index(key)] = newValues[newKeys.index(key)] + values[i]
+        
+        res = dict(zip(newKeys, newValues))
 
     with open(working_dir + '/data/german/' + outputfile, 'w') as w:
         sort = sorted(res.items(), key=lambda x: x[1], reverse=False)
